1dd176104443ff8e5cb3e07145a3f8e5ce82ef64
Raflifauzanrauf <raflifauzanr@gmail.com>
2025-01-02 10:18:28 +0700

    updateHistory

---
+++ lib/pages/historypage.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+class Historypage extends StatefulWidget {
+  const Historypage({super.key});
+
+  @override
+  State<Historypage> createState() => _HistorypageState();
+}
+
+class _HistorypageState extends State<Historypage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'HISTORY',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start, 
+          children: [
+            Text(
+              'November 2004',
+              style: TextStyle(
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10),
+            Container(
+              padding: EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                color: Colors.red,
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center, 
+                mainAxisAlignment: MainAxisAlignment.spaceBetween, 
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(right: 16), 
+                    child: Icon(Icons.access_alarms_outlined, color: Colors.white),
+                  ),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start, 
+                      children: [
+                        Text(
+                          'Learning Code',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                        Text(
+                          'Lorem ipsum dolor sit amet, consectetur',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w300,
+                            color: Colors.white,
+                            fontSize: 12,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(left: 16), 
+                    child: Icon(Icons.more_vert, color: Colors.white),
+                  ),
+                ],
+              ),
+            )
+
+
+          ],
+        ),
+      ),
+    );
+  }
+}
